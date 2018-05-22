cdd4292e1eda87dc9037317669699aefecbef065
Steven <steven@rootedlogics.com>
2018-05-22 12:20:37 -0400

    some middleware

--- server.js
+++ server.js
@@ -1,11 +1,36 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
+
 var app = express();
 
 hbs.registerPartials(__dirname + "/views/partials");
 
-app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'hbs');
+
+/**
+ * Middleware
+ */
+
+// Server log
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + "\n", (err) => {
+    if (err) {
+      console.log('Unable to append to server log.');
+    }
+  });
+  next();
+});
+
+// Maintenance
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear();
