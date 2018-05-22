10bc9ccb894f2b5a4f9811d323f217048a1268bf
Steven <steven@rootedlogics.com>
2018-05-22 12:00:08 -0400

    helper function for date

--- server.js
+++ server.js
@@ -2,23 +2,27 @@ const express = require('express');
 const hbs = require('hbs');
 var app = express();
 
+hbs.registerPartials(__dirname + "/views/partials");
+
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'hbs');
 
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
 
 app.get('/', (req, res) => {
   // res.send('<h1>Hello Express</h1>');
   res.render('home.hbs', {
     pageTitle: 'Home Page',
     welcomeMessage: "Welcome to my website",
-    currentYear: new Date().getFullYear()
   });
 });
+
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About Page',
-    currentYear: new Date().getFullYear()
   });
 });
 
