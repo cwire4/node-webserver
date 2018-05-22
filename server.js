3cdf523324462f97c19fd4a3aab2b97654524113
Steven <steven@rootedlogics.com>
2018-05-22 11:31:24 -0400

    setting up templates

--- server.js
+++ server.js
@@ -1,26 +1,24 @@
 const express = require('express');
-
+const hbs = require('hbs');
 var app = express();
+
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'hbs');
+
 
 app.get('/', (req, res) => {
   // res.send('<h1>Hello Express</h1>');
-  res.send({
-    name: 'Steven',
-    likes: [
-      'Hockey',
-      'Solidity'
-    ]
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: "Welcome to my website",
+    currentYear: new Date().getFullYear()
   });
 });
 
 app.get('/about', (req, res) => {
-  // res.send('<h1>Hello Express</h1>');
-  res.send({
-    name: 'About',
-    likes: [
-      'Hockey',
-      'Solidity'
-    ]
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
   });
 });
 
@@ -32,4 +30,6 @@ app.get('/bad', (req, res) => {
   });
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log('Server is up on port 3000.');
+});
